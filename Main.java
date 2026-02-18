58a6002661188b84e05628bb7406ec38cd3d80bb
Thirumalai23013035 <153185249+Thirumalai23013035@users.noreply.github.com>
2026-02-18 23:09:18 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        // User obj = new User(1,"Thirumalai",18,"7339413624","Student");
+        // User obj = new User(1,"Thirumalai",20,"7339413624","Student");
         // Bus_Pass obj2 = new Bus_Pass(1,1,"Krishnagiri", "Student", 30);
 
         // obj.display_User();
